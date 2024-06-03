36b5357d3cef84c3111e597c241cc9e6a100bff4
Akash Das <95500036+MrfoxAK@users.noreply.github.com>
2024-06-03 17:55:39 +0530

    Add files via upload

---
+++ Trail_of_ones.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+
+int numberOfConsecutiveOnes(int n) {
+     int arr[n];
+     
+}
+
+int main()
+{
+     int n = 2;
+     int total = pow(2,n);
+
+     return 0;
+}
